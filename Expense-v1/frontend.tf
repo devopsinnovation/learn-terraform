01e9e771bbc4d789e556f16c656bbd79b2d0c13d
aditya <adityavittal92@gmail.com>
2024-05-18 21:52:46 +0530

    updated the url for the frontend bavkend and the mysql

--- Expense-v1/frontend.tf
+++ Expense-v1/frontend.tf
@@ -1,7 +1,12 @@
 resource "aws_instance" "frontend" {
   ami           = "ami-090252cbe067a9e58"
   instance_type = "t2.small"
+
   tags = {
-    Name=frontendserver
+    name="frontendserver"
   }
+}
+
+resource "aws_iam_role" "workstation-role" {
+  assume_role_policy = "workstation-role"
 }
