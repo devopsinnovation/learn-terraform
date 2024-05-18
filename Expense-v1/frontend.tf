bf8227a7c1a9fa38b205b37cd952df7a0eb41b57
aditya <adityavittal92@gmail.com>
2024-05-18 21:58:31 +0530

    updated the url for the frontend bavkend and the mysql

--- Expense-v1/frontend.tf
+++ Expense-v1/frontend.tf
@@ -1,4 +1,5 @@
 resource "aws_instance" "frontendserver" {
+  name = "frontendserver"
   ami           = "ami-090252cbe067a9e58"
   instance_type = "t2.small"
   subnet_id = "subnet-042cbd7641215cdce"
