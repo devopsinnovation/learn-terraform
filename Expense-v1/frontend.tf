c7863ab1f0ba7c0cad54ce549f605623070c7741
aditya <adityavittal92@gmail.com>
2024-05-20 15:04:07 +0530

    updated the url for the frontend bavkend and the mysql

--- Expense-v1/frontend.tf
+++ Expense-v1/frontend.tf
@@ -1,31 +1,16 @@
 resource "aws_instance" "frontendserver" {
+  count =  length(var.component)
   ami           = "ami-090252cbe067a9e58"
   instance_type = "t2.small"
   subnet_id = "subnet-042cbd7641215cdce"
   vpc_security_group_ids = ["sg-0706ec15d6ace75e5"]
 
   tags = {
-    Name="frontendserver"
+    Name= var.component[count.index]
   }
 }
 
-resource "aws_instance" "backendserver" {
-  ami           = "ami-090252cbe067a9e58"
-  instance_type = "t2.small"
-  subnet_id = "subnet-042cbd7641215cdce"
-  vpc_security_group_ids =  ["sg-0706ec15d6ace75e5"]
+variable "component" {
 
-  tags = {
-    Name="backendserver"
-  }
-}
-resource "aws_instance" "mysqlserver" {
-  ami           = "ami-090252cbe067a9e58"
-  instance_type = "t2.small"
-  subnet_id = "subnet-042cbd7641215cdce"
-  vpc_security_group_ids =  ["sg-0706ec15d6ace75e5"]
-
-  tags = {
-    Name="mysqlserver"
-  }
+  default = ["frontendserver", "backendserver", "mysqlserver"]
 }
